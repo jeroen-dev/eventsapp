a49c6c0075809d5268866c7b985f8d687ac23df7
Jeroen <jeroentheunissen@hotmail.com>
2020-09-30 16:22:04 +0200

    added Redux Thunk, async/await, calendar UI and filters UI, loading compnent

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import SignedOutMenu from './SignedOutMenu';
 import SignedInMenu from './SignedInMenu';
--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { increment, decrement } from './testReducer';
 import { openModal } from '../../app/common/modals/modalReducer';
 
 export default function Sandbox() {
+  const [target, setTarget] = useState(null);
   const dispatch = useDispatch();
   const data = useSelector((state) => state.test.data);
+  const { loading } = useSelector((state) => state.async);
   return (
     <>
       <h1>Testing 123</h1>
       <h3>The data is: {data}</h3>
       <Button
+        name='increment'
         content='increment'
         color='green'
-        onClick={() => dispatch(increment(20))}
+        onClick={(e) => {
+          dispatch(increment(20));
+          setTarget(e.target.name);
+        }}
+        loading={loading && target === 'increment'}
       />
       <Button
+        name='decrement'
         content='decrement'
         color='red'
-        onClick={() => dispatch(decrement(10))}
+        onClick={(e) => {
+          dispatch(decrement(10));
+          setTarget(e.target.name);
+        }}
+        loading={loading && target === 'decrement'}
       />
       <Button
         content='open modal'
